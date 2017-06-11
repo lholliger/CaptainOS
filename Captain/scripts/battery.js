77d5b6c7337ffb266fe35e141a9e9a3039a94315
DatOneLefty <k9dogs101@gmail.com>
2017-06-11 00:55:04 -0400

    battery precentage

---
+++ Captain/scripts/battery.js
@@ -0,0 +1,6 @@
+var bat = battery.level * 100 + "%";
+document.getElementById("bcurrent").style.width = bat;
+setInterval(function() {
+  var bat = battery.level * 100 + "%";
+  document.getElementById("bcurrent").style.width = bat;
+}, 5000);
