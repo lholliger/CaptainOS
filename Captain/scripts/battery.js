d9a89404d68f4cba2c2871b327333d196e0d507e
DatOneLefty <k9dogs101@gmail.com>
2017-06-11 07:29:40 -0400

    low battery bug fix

--- Captain/scripts/battery.js
+++ Captain/scripts/battery.js
@@ -2,7 +2,7 @@ navigator.getBattery().then(function(result) {
   console.log(result);
   document.getElementById("bcurrent").style.width = result["level"] * 100 + "%";
 
-  if (result["level"] < 15) {
+  if (result["level"] * 100 < 15) {
     document.getElementById("bcurrent").style.backgroundColor = "red";
   }
   if (result["charging"] == true) {
@@ -15,7 +15,7 @@ setInterval(function() {
     console.log(result);
     document.getElementById("bcurrent").style.width = result["level"] * 100 + "%";
 
-    if (result["level"] < 15) {
+    if (result["level"] * 100 < 15) {
       document.getElementById("bcurrent").style.backgroundColor = "red";
     }
 
