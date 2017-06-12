8db770ce44734495901bf5974969ba8fb69187b7
DatOneLefty <k9dogs101@gmail.com>
2017-06-12 00:25:18 -0400

    home button stuff

---
+++ Captain/scripts/buttons.js
@@ -0,0 +1,9 @@
+function lockscreenmain() {
+  document.getElementById("lockscreen").style.filter = "";
+  document.getElementById("pass").style.display = "none";
+  document.getElementById("slider").style.display = "block";
+  var finalcode = "";
+  var showdisplay = "";
+  document.getElementById("currentpassfill").innerHTML = "&nbsp;"  + showdisplay + "&nbsp;";
+
+}
